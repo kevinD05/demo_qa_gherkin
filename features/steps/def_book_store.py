7c0a450658498a99eff60fc88c0301f088771831
kevinD05 <alexanderkevin05@gmail.com>
2023-08-14 14:07:41 -0600

    report

--- features/steps/def_book_store.py
+++ features/steps/def_book_store.py
@@ -4,20 +4,18 @@ from helper.pages.page_book_store import pagebookstore
 import time
 from selenium.webdriver.common.by import By
 
-
-
 @step(u'cargue la pagina, damos click al boton "book store application"')
-def step_impl(context):
+def step_load_page_and_click_book_store(context):
     Basepage.click_button(context, pagebookstore.btn_book_store)
     time.sleep(2)
 
 @step(u'damos click al boton \'book store\'')
-def step_impl(context):
+def step_click_book_store(context):
     Basepage.click_button(context, pagebookstore.btn_store)
     time.sleep(2)
 
 @step(u'ingresamos el user y password correspondiente \'<user>\'')
-def step_impl(context):
+def step_login(context):
     login = context.find_element(By.XPATH, '//*[@id="login"]')
     login.click()
     username = context.find_element(By.XPATH, '//*[@id="userName"]')
@@ -28,23 +26,22 @@ def step_impl(context):
     password.send_keys('Alaputaestacontra5556%')
     time.sleep(2)
 
-
 @step(u'damos click al libro con el nombre \' Working Introduction\'')
-def step_impl(context):
+def step_click_book1(context):
     Basepage.click_button(context, pagebookstore.book1)
 
 @step(u'damos click al boton \'add to your collection\'')
-def step_impl(context):
+def step_click_add_button(context):
     Basepage.click_button(context, pagebookstore.btn_add)
 
 @step(u'damos click al libro con el nombre \'Harnessing the Power of the Web\'')
-def step_impl(context):
+def step_click_book2(context):
     Basepage.click_button(context, pagebookstore.book2)
 
-@step(u'damos click al boton \'add to your collection\'')
-def step_impl(context):
+#@step(u'damos click al boton \'add to your collection\'')
+def step_click_add_button_2(context):
     Basepage.click_button(context, pagebookstore.btn_add)
 
 @step(u'Validamos que los libros se guarden en nuestro perfil')
-def step_impl(context):
+def step_validate_books_in_profile(context):
     pass
--- features/web/Elements/Dynamic_properties.feature
+++ features/web/Elements/Dynamic_properties.feature
@@ -6,6 +6,6 @@ Scenario:Validar dar click en los links
     When cargue la pagina, damos click al boton "Elements"
     And damos click al boton "Dynamic properties"
     And damos click al boton "color change"
-    And esperaremos 5 segundos hasta que el boton 'will enable 5 seconds" sea visible y daremos click
+    And esperaremos 5 segundos hasta que el boton 'will enable 5 seconds sea visible y daremos click'
     And damos click en el boton "visible after 5 seconds" despues de 5 segundos
     Then Validamos que al dar click en los botones el cortono cambie

ad8a7abfc4bd43e04641c82d06095cb96c82122c
kevinD05 <alexanderkevin05@gmail.com>
2023-09-04 13:00:01 -0600

    element

---
+++ features/steps/def_element.py
@@ -0,0 +1,22 @@
+from behave import step
+import os
+import time
+from helper.services.acciones_basicas import Basepage
+from helper.pages.page_element import PageDemoQA
+
+
+@step(u'Ingresamos a la url "URL_QA"')
+def step_impl(context):
+    url = os.getenv('URL_QA')
+    context.browser.get(url)
+    time.sleep(3)
+
+@step(u'cargue la pagina, damos click al boton "Elements"')
+def step_impl(context):
+    Basepage.click_button(context, PageDemoQA.btn_elements)
+    time.sleep(7)
+
+@step(u'validos que nos mande a las opcions de elementos')
+def step_impl(context):
+    elemento = Basepage.validarElementoExistente(context, PageDemoQA.btn_text_box)
+    assert elemento == True, "El elemento no existe"
